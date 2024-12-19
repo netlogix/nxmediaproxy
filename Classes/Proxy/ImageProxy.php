e77a9c7b2aaf275bdabdfdc07a78e2bac21603ed
Sascha Nowak <sascha.nowak@netlogix.de>
2024-12-19 17:57:11 +0100

    feat: update to TYPO3 13.4

--- Classes/Proxy/ImageProxy.php
+++ Classes/Proxy/ImageProxy.php
@@ -2,72 +2,64 @@
 
 namespace Netlogix\Nxmediaproxy\Proxy;
 
+use Psr\Http\Message\ServerRequestInterface;
 use Throwable;
 use TYPO3\CMS\Core\Http\ImmediateResponseException;
 use TYPO3\CMS\Core\Http\RedirectResponse;
-use TYPO3\CMS\Core\Http\ServerRequest;
 use TYPO3\CMS\Core\Resource\Exception\OnlineMediaAlreadyExistsException;
 use TYPO3\CMS\Core\Resource\OnlineMedia\Helpers\OnlineMediaHelperRegistry;
 use TYPO3\CMS\Core\Resource\ProcessedFile;
 use TYPO3\CMS\Core\Resource\ResourceFactory;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Frontend\Controller\ErrorController;
 
-class ImageProxy
+final readonly class ImageProxy
 {
-
-    /**
-     * @var ServerRequest
-     */
-    private $request;
-
-    public function __construct()
-    {
-        // Inject request from globals until request will be available to cObj
-        $this->request = $GLOBALS['TYPO3_REQUEST'];
+    public function __construct(
+        private ResourceFactory $resourceFactory,
+        private OnlineMediaHelperRegistry $helper,
+        private ErrorController $errorController,
+    ) {
     }
 
-    public function youtube()
+    public function youtube(string $content, array $conf, ServerRequestInterface $request)
     {
-        $queryParams = $this->request->getQueryParams();
-        if (!array_key_exists('video-id', $queryParams) && !array_key_exists('id', $queryParams)) {
+        $queryParams = $request->getQueryParams();
+        if (!array_key_exists('video-id', $queryParams)) {
             exit;
         }
 
-        $id = $queryParams['video-id'] ?? $queryParams['id'];
-        $this->redirectToThumbnail("https://youtu.be/{$id}");
+        $id = $queryParams['video-id'];
+        $this->redirectToThumbnail("https://youtu.be/{$id}", $request);
     }
 
-    public function vimeo()
+    public function vimeo(string $content, array $conf, ServerRequestInterface $request)
     {
-        $queryParams = $this->request->getQueryParams();
-        if (!array_key_exists('video-id', $queryParams) && !array_key_exists('id', $queryParams)) {
+        $queryParams = $request->getQueryParams();
+        if (!array_key_exists('video-id', $queryParams)) {
             exit;
         }
 
-        $id = $queryParams['video-id'] ?? $queryParams['id'];
-        $this->redirectToThumbnail("https://vimeo.com/{$id}");
+        $id = $queryParams['video-id'];
+        $this->redirectToThumbnail("https://vimeo.com/{$id}", $request);
     }
 
-    private function redirectToThumbnail(string $url)
+    private function redirectToThumbnail(string $url, ServerRequestInterface $request)
     {
-        $resourceFactory = GeneralUtility::makeInstance(ResourceFactory::class);
-        $storage = $resourceFactory->getStorageObject(0);
+        $storage = $this->resourceFactory->getStorageObject(0);
         if (!$storage->hasFolder('typo3temp/assets/online_media_proxy')) {
             $storage->createFolder('typo3temp/assets/online_media_proxy');
         }
         $folder = $storage->getFolder('typo3temp/assets/online_media_proxy');
 
-        $helper = GeneralUtility::makeInstance(OnlineMediaHelperRegistry::class);
         try {
             try {
-                $file = $helper->transformUrlToFile($url, $folder);
+                $file = $this->helper->transformUrlToFile($url, $folder);
             } catch (OnlineMediaAlreadyExistsException $e) {
                 $file = $e->getOnlineMedia();
             }
 
-            $previewImage = GeneralUtility::makeInstance(ResourceFactory::class)->retrieveFileOrFolderObject(
-                $helper->getOnlineMediaHelper($file)->getPreviewImage($file)
+            $previewImage = $this->resourceFactory->retrieveFileOrFolderObject(
+                $this->helper->getOnlineMediaHelper($file)->getPreviewImage($file)
             );
 
             $previewThumbnail = $previewImage->process(ProcessedFile::CONTEXT_IMAGECROPSCALEMASK, [
@@ -75,17 +67,15 @@ class ImageProxy
             ]);
             $response = new RedirectResponse($previewThumbnail->getPublicUrl());
         } catch (Throwable) {
-            $response = GeneralUtility::makeInstance(ErrorController::class)->pageNotFoundAction(
-                $this->request,
-                'Video not found',
-            );
+            $response = $this->errorController->pageNotFoundAction($request, 'Video not found');
         }
 
         // Cache 404 and redirect response for short amount of time to prevent DoS attacks
-        if (!empty($GLOBALS['TSFE']->config['config']['sendCacheHeaders'])) {
-            $response = $response->withHeader('Expires', gmdate('D, d M Y H:i:s T', $GLOBALS['EXEC_TIME'] + 60));
-            $response = $response->withHeader('Cache-Control', 'public, max-age=60');
-            $response = $response->withHeader('Pragma', 'public');
+        if ($request->getAttribute('frontend.cache.instruction')->isCachingAllowed()) {
+            $response = $response
+                ->withHeader('Expires', gmdate('D, d M Y H:i:s T', $GLOBALS['EXEC_TIME'] + 60))
+                ->withHeader('Cache-Control', 'max-age=0, s-maxage=60')
+                ->withHeader('Pragma', 'public');
         }
 
         throw new ImmediateResponseException($response, 1660655670);
